a0f53f811b1729a4a32390c042e873736f7520bd
oguzBatur <sariozbatur@gmail.com>
2021-09-04 19:58:52 +0300

    Added some quotes and button now works!

--- app.js
+++ app.js
@@ -7,39 +7,24 @@ const quotes = [
     ['Baykuştan korkumuz yok! biz ki, şahinler sürüsü.','Fatih Sultan Mehmet'],
     ['Dün akıllıydım, dünyayı değiştirmek istedim; Bugün ise bilgeyim, Kendimi Değiştirdim.','Mevlana'],
     ['Köpeklerin kardeşliği, aralarına kemik atana kadardır.','Mevlana'],
-    ['',''],
-    ['',''],
-    ['',''],
-    ['',''],
-    ['',''],
-    ['',''],
-    ['',''],
-    ['',''],
-    ['',''],
-    ['',''],
-    ['',''],
-    ['',''],
-    ['',''],
-    ['',''],
-    ['',''],
-    ['',''],
-    ['',''],
-    ['',''],
-    ['',''],
-    ['',''],
-    ['',''],
-    ['',''],
-    ['',''],
-    ['',''],
-    ['',''],
-    ['',''],
-    ['',''],
-    ['',''],
-    ['',''],
-    ['',''],
-    ['',''],
-    ['',''],
-    ['',''],
-    ['','']
+    ['Biz Türk milleti temiz bir milletiz. Biat nedir asla bilmeyiz. Bundan dolayı Tanrı bizi aziz kılmıştır. ','Alparslan Türkeş'],
+    ['Eğer birgün benim sözlerim bilimle ters düşerse, bilimi seçin.','Mustafa Kemal Atatürk'],
+    ['Söz konusu olan vatansa, gerisi teferruat.','Mustafa Kemal Atatürk'],
+    ['Bağımsızlıktan yoksun bir ulus, uygar insanlık karşısında uşak olmaktan kurtulamaz.','Mustafa Kemal Atatürk'],
+    ['İstikbal göklerdedir.','Mustafa Kemal Atatürk.'],
+    ['Tarih yazmak korkaklara göre bir iş değildir.','Fatih Sultan Mehmet'],
+    ['Benim kudretimin ulaştığı yere onların hayalleri bile ulaşamaz.','Fatih Sultan Mehmet'],
+    ['Hakiki sanat muhteşem bir şehir vücuda getirmek ve halkının kalbini saadetler doldurmaktır.','Fatih Sultan Mehmet'],
+];
+const author = document.getElementById('author-container');
+const quoteText = document.getElementById('quote');
 
-]
+
+
+btn.addEventListener('click', () => {
+
+    let randomNum = Math.floor(Math.random() * quotes.length);
+
+    quoteText.textContent = quotes[randomNum][0];
+    author.textContent = quotes[randomNum][1];
+})
